dfc22deb7cbca8b5a8e61ece998441857156ae5b
Ttae27 <ponghong.2744@gmail.com>
2025-07-02 00:46:40 +0700

    [add] error status

--- app.py
+++ app.py
@@ -4,26 +4,33 @@ from models import Item, Transaction, TransactionDetail
 from schemas import CreateTransaction
 
 def save_transaction(db: Session, transactions: list[CreateTransaction]):
-    for transaction in transactions:
-        detail_list = []
-        for detail in transaction.details:
-            kanom = db.query(Item).filter_by(barcode=detail.barcode).first()
-            kanom.quantity -= detail.quantity
-            db.add(kanom)
+    try:
+        for transaction in transactions:
+            detail_list = []
+            for detail in transaction.details:
+                kanom = db.query(Item).filter_by(barcode=detail.barcode).first()
+                kanom.quantity -= detail.quantity
+                db.add(kanom)
 
-            trans_detail = TransactionDetail(quantity=detail.quantity, subtotal=detail.subtotal, item=kanom)
-            detail_list.append(trans_detail)
-        trans = Transaction(date=transaction.date, time=transaction.time, total=transaction.total)
-        trans.transaction_detail.extend(detail_list)
+                trans_detail = TransactionDetail(quantity=detail.quantity, subtotal=detail.subtotal, item=kanom)
+                detail_list.append(trans_detail)
+            trans = Transaction(date=transaction.date, time=transaction.time, total=transaction.total)
+            trans.transaction_detail.extend(detail_list)
 
-        db.add_all(detail_list)
-        db.add(trans)
-    db.commit()
+            db.add_all(detail_list)
+            db.add(trans)
+        db.commit()
 
-    return "Successfully save transaction"
+        return "Successfully save transaction"
+    except:
+        db.rollback()
+        return "Failed to save transaction"
 
 def get_items(db: Session):
     items = db.query(Item).all()
+    if not items:
+        raise HTTPException(status_code=404, detail="Item not found")
+
     return items
 
 def get_transaction(db: Session, id: int):
@@ -33,12 +40,14 @@ def get_transaction(db: Session, id: int):
     
     temp = db.query(TransactionDetail).filter_by(transaction_id=id).all()
     if not temp:
-        raise HTTPException(status_code=404, detail="Transaction  not found")
+        raise HTTPException(status_code=404, detail="TransactionDetail not found")
     
     transaction.extend(temp)
     return transaction
 
 def get_transactions(db: Session):
     transaction = db.query(Transaction).all()
+    if not transaction:
+        raise HTTPException(status_code=404, detail="Transactions not found")
 
     return transaction
